4033917ec47bd981bfd54284d8f3972778fae57f
Grzegorz Kunc <grzeg.kunc@gmail.com>
2020-03-07 19:08:43 +0100

    Add new filds with getters and setters

--- src/models/Circle.java
+++ src/models/Circle.java
@@ -5,13 +5,40 @@ import interfaces.IShapes;
 public class Circle implements IShapes {
 
     private String class_name;
+    private double x_position, y_position;
+    private double width, height;
 
     public String getClass_name() {
         return this.class_name;
     }
-
-    public void setClass_name() {
+    public void setClass_name(String class_name) {
         this.class_name = class_name;
+    }
+
+    public double getX_position() {
+        return this.x_position;
+    }
+    public void setX_position(double x_position) { this.x_position = x_position; }
+
+    public double getY_position() {
+        return this.y_position;
+    }
+    public void setY_position(double y_position) {
+        this.y_position = y_position;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
     }
 
     public Circle() {
